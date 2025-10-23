7dc7d737691305a46ec829ed9f942260e489bee8
Calvin Kimani <calvinkimani475@students.tukenya.ac.ke>
2025-10-23 15:07:19 +0300

    refactor: improve ownership checks and error handling in FundMe contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,29 +3,36 @@ pragma solidity ^0.8.0;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error ErrNotOwner();
+error ErrWithdrawFail();
+error ErrMinimumDonation();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minDonationUsd = 5 * 1e18;
-    address public owner;
+    uint256 public constant MIN_DONATION_AMT = 5 * 1e18;
+    address public immutable i_owner;
     mapping(address donator => uint256 amount) public donations;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     modifier isOwner(){
-      require(msg.sender == owner);
+      if(msg.sender != i_owner)
+        revert(ErrNotOwner());
       _;
     }
 
     function fund() public payable {
-      require(msg.value.convertToUsd() >= minDonationUsd, "Minimum donation not met");
+      require(msg.value.convertToUsd() >= MIN_DONATION_AMT, "Minimum donation not met");
       donations[msg.sender] += msg.value;
     }
 
     function withdraw() public isOwner {
-      (bool success,) = payable(owner).call{value: address(this).balance}("");
-      require(success, "Withdraw fail");
+      (bool success,) = payable(i_owner).call{value: address(this).balance}("");
+      if(!success){
+        revert(ErrWithdrawFail());
+      }
     }
 }
